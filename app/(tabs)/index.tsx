7b91e8397622eda54d0b5e58340b8ec3aa2a7c49
Thien An Tran <fxthienan@gmail.com>
2025-01-21 19:05:41 -0800

    chore(config): configure NativeWind styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className='text-white font-bold text-3xl'>Home</Text>
     </View>
   );
 }
