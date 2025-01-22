f6bcc9cca46ebea5152813dcab42cd35b8adeba9
Thien An Tran <fxthienan@gmail.com>
2025-01-21 22:21:00 -0800

    feat: add auth screen and more tabs on main screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,12 +7,44 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: "#000", 
         headerShown: false,
+        tabBarIconStyle: {
+          width: 'auto',  // Allow text to take necessary width
+          height: 24,     // Set consistent height
+        },
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
           tabBarIcon: () => <Text>Home</Text>,
+        }}
+      />
+      <Tabs.Screen
+        name="friends"
+        options={{
+          title: 'Friends',
+          tabBarIcon: () => <Text>Friends</Text>,
+        }}
+      />
+      <Tabs.Screen
+        name="camera"
+        options={{
+          title: 'Camera',
+          tabBarIcon: () => <Text>Camera</Text>,
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          title: 'Inbox',
+          tabBarIcon: () => <Text>Inbox</Text>,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: () => <Text>Profile</Text>,
         }}
       />
     </Tabs>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout() {
 
   return (
     <Stack>
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
