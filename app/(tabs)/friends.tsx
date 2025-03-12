4e5dbd718b848af42da3b538027a560bf9eb32b6
Thien An Tran <fxthienan@gmail.com>
2025-03-11 22:32:15 -0700

    feat: add friends feed to show videos from users who follow each other

--- app/(tabs)/friends.tsx
+++ app/(tabs)/friends.tsx
@@ -1,9 +1,87 @@
-import { Text, View } from 'react-native';
+import React, { useEffect, useState, useRef } from "react";
+import { View, FlatList, Dimensions, Text, SafeAreaView } from "react-native";
+import { supabase } from "@/utils/supabase";
+import VideoDisplay from "@/components/VideoDisplay";
+import Header from "@/components/Header";
+import { useAuth } from "@/providers/AuthProvider";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function () {
+  const { friends } = useAuth();
+  const [videos, setVideos] = useState<any[]>([]);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  useEffect(() => {
+    getVideos();
+  }, [friends]);
+
+  const getVideos = async () => {
+    const { data, error } = await supabase
+      .from("Video")
+      .select("*, User(username, id)")
+      .in("user_id", friends)
+      .order("created_at", { ascending: false });
+
+    getSignedUrls(data);
+  };
+
+  const getSignedUrls = async (videos: any[]) => {
+    const { data, error } = await supabase.storage
+      .from("videos")
+      .createSignedUrls(
+        videos.map((video) => video.uri),
+        60 * 60 * 24 * 7
+      );
+
+    let videosUrls = videos?.map((item) => {
+      item.signedUrl = data?.find(
+        (signedUrl) => signedUrl.path === item.uri
+      )?.signedUrl;
+      return item;
+    });
+
+    console.log(videosUrls);
+    setVideos(videosUrls);
+  };
+
+  const viewabilityConfig = useRef({
+    itemVisiblePercentThreshold: 50, // Item must be 50% visible to trigger
+    minimumViewTime: 0,
+  }).current;
+
+  const onViewableItemsChanged = useRef(
+    ({ viewableItems }: { viewableItems: any[] }) => {
+      if (viewableItems.length > 0) {
+        setActiveIndex(viewableItems[0].key);
+      }
+    }
+  ).current;
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-black font-bold text-3xl'>Friends</Text>
+    <View className="flex-1 items-center justify-center bg-black">
+      <View className="absolute top-16 left-0 right-0 z-10">
+        <Header title="Friends" color="white" search />
+      </View>
+      <FlatList
+        data={videos}
+        snapToInterval={Dimensions.get("window").height}
+        snapToStart
+        decelerationRate="fast"
+        // keyExtractor={(item) => item.id.toString()}
+        viewabilityConfig={viewabilityConfig}
+        onViewableItemsChanged={onViewableItemsChanged}
+        renderItem={({ item }) => (
+          <VideoDisplay videoItem={item} isViewable={activeIndex === item.id} />
+        )}
+        ListEmptyComponent={
+          <SafeAreaView className="flex-1 mt-72 items-center justify-center bg-black">
+            <Ionicons name="sad" size={50} color="white" />
+            <Text className="text-white text-3xl font-bold">
+              No videos found...
+            </Text>
+          </SafeAreaView>
+        }
+      />
     </View>
   );
 }
