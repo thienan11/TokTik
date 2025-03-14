a2d1eb356590978bcb5b75badc26c37358960754
Thien An Tran <fxthienan@gmail.com>
2025-03-13 22:13:51 -0700

    feat: display likes and videos in profile page

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -4,11 +4,14 @@ import {
   TouchableOpacity,
   Image,
   SafeAreaView,
+  FlatList,
+  Dimensions,
 } from "react-native";
 import { useAuth } from "@/providers/AuthProvider";
 import { supabase } from "@/utils/supabase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
+import { useVideoPlayer, VideoView } from "expo-video";
 
 export default function ({
   user,
@@ -26,6 +29,8 @@ export default function ({
     getFollowing,
   } = useAuth();
   const [profilePic, setProfilePic] = useState<string>("");
+  const [videos, setVideos] = useState<any[]>([]);
+  const [likes, setLikes] = useState<any[]>([]);
 
   // useEffect(() => {
   //   const loadAvatar = async () => {
@@ -40,6 +45,48 @@ export default function ({
 
   //   loadAvatar();
   // }, [user?.id, avatarUrl, getUserAvatar]);
+
+  useEffect(() => {
+    getVideos();
+    getLikes();
+  }, [user]);
+
+  const getVideos = async () => {
+    const { data, error } = await supabase
+      .from("Video")
+      .select("*, User(username, id)")
+      .eq("user_id", user?.id)
+      .order("created_at", { ascending: false })
+      .limit(3);
+    getSignedUrls(data);
+  };
+
+  const getSignedUrls = async (videos: any[]) => {
+    const { data, error } = await supabase.storage
+      .from("videos")
+      .createSignedUrls(
+        videos.map((video) => video.uri),
+        60 * 60 * 24 * 7
+      );
+
+    let videosUrls = videos?.map((item) => {
+      item.signedUrl = data?.find(
+        (signedUrl) => signedUrl.path === item.uri
+      )?.signedUrl;
+      return item;
+    });
+
+    console.log(videosUrls);
+    setVideos(videosUrls);
+  };
+
+  const getLikes = async () => {
+    const { data, error } = await supabase
+      .from("Like")
+      .select("*")
+      .eq("video_user_id", user?.id);
+    setLikes(data);
+  };
 
   const pickImage = async () => {
     if (authUser?.id !== user?.id) return; // if user is not you, they can't update your profile pic
@@ -104,6 +151,27 @@ export default function ({
     return "https://placehold.co/40x40"; // Default placeholder
   };
 
+  const VideoItem = ({ url }: { url: string }) => {
+    const player = useVideoPlayer(url, (player) => {
+      player.loop = true;
+    });
+
+    return (
+      <VideoView
+        style={{
+          width: Dimensions.get("window").width / 3 - 4, // Adjusted width with spacing
+          height: 200,
+          margin: 2, // Small margin for spacing between videos
+          borderRadius: 10,
+          overflow: "hidden",
+        }}
+        player={player}
+        contentFit="cover"
+        nativeControls={false}
+      />
+    );
+  };
+
   return (
     <SafeAreaView className="flex-1 items-center">
       <TouchableOpacity onPress={pickImage}>
@@ -124,7 +192,7 @@ export default function ({
         </View>
         <View className="flex-1 items-center justify-center">
           <Text className="text-xl font-semibold">Likes</Text>
-          <Text className="text-xl ">1000</Text>
+          <Text className="text-xl ">{likes.length}</Text>
         </View>
       </View>
       {authUser?.id === user?.id ? (
@@ -159,6 +227,23 @@ export default function ({
             </TouchableOpacity>
           )}
         </View>
+      )}
+      {videos.length > 0 ? (
+        <FlatList
+          numColumns={3}
+          data={videos}
+          initialNumToRender={6} // Lazy loading for better performance
+          keyExtractor={(item) => item.id.toString()}
+          scrollEnabled={false}
+          renderItem={({ item }) => <VideoItem url={item.signedUrl} />}
+          getItemLayout={(data, index) => ({
+            length: 200,
+            offset: 200 * index,
+            index,
+          })}
+        />
+      ) : (
+        <Text className="text-gray-500 text-lg my-4">No videos available</Text>
       )}
     </SafeAreaView>
   );
